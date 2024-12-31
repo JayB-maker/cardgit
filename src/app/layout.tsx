0b7c0905a91bced87a860f72700811a2697ffc47
abiodun <ajiboyeabiodun001@gmail.com>
2024-12-31 12:07:22 +0100

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const clientId = (process.env.NEXT_APP_GOOGLE_CLIENT_ID as string) || "";
+  // const clientId = (process.env.NEXT_APP_GOOGLE_CLIENT_ID as string) || "";
 
   return (
     <html lang="en">
@@ -32,10 +32,10 @@ export default function RootLayout({
             </div>
           }
         >
-          <GoogleOAuthProvider clientId={clientId}>
+          {/* <GoogleOAuthProvider clientId={clientId}> */}
             {children}
             <ToastContainer position="bottom-right" />
-          </GoogleOAuthProvider>
+          {/* </GoogleOAuthProvider> */}
         </Suspense>
         {/* </SessionProvider> */}
       </body>
